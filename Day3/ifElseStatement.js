d90d2fbff85973d35ed7aef46465ba478ea0ff29
ADARSH MISHRA <mishraadarshkumar2000@gmail.com>
2024-07-17 23:59:32 +0530

    added task for day 3

---
+++ Day3/ifElseStatement.js
@@ -0,0 +1,46 @@
+// ***************************        task 1         ****************************
+// program to find a number wheather it is PromiseRejectionEvent,negative or zero
+
+let number=45;
+if(number>0)
+    console.log("Number is  positive")
+else if(number<0)
+    console.log("Number is Negative")
+else 
+    console.log("Number is Zero")
+
+
+    //***********************          Task 2        ********************************
+    // program to check weather a person is eligible to vote or not
+
+    const age=23;
+    if(age>=18)
+        console.log("You can Vote")
+    else console.log("You can't vote")
+
+
+   //***********************          Task 2        ********************************   
+
+//    write a program to find the largest among three numbers  by this we can learn nested if else statements
+const num1=23;
+const num2=13;
+const num3=43;
+if(num1>num2)
+{
+    if(num1>num3)
+        console.log("num1 is largest among all the three numbers")
+    
+}
+if(num2>num1)
+{
+    if(num2>num3)
+        console.log("num2 is largest among all the three numbers")
+    
+}
+if(num3>num1)
+{
+    if(num3>num2)
+        console.log("num3 is largest among all the three numbers")
+    
+}
+    
